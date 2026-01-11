95bde796c5edc81fca2643659e4efc36986ed514
Arthur Bied-Charreton <arthur@biedcharreton.com>
2026-01-11 10:04:26 +0100

    Add reserved keywords and proper comments

--- src/lexer.rs
+++ src/lexer.rs
@@ -22,15 +22,12 @@ impl<'lex> Lexer<'lex> {
     pub fn tokenize(&mut self) -> &Vec<Token<'_>> {
         while self.curr < self.source.len() {
             self.start = self.curr;
-            let ch = self.advance();
-            match ch {
+            let Some(c) = self.advance() else {
+                return &self.tokens;
+            };
+            match c {
                 b'\n' => self.line += 1,
                 ch if ch.is_ascii_whitespace() => (),
-                b'#' => {
-                    while self.peek() != b'\n' && self.curr < self.source.len() {
-                        self.advance();
-                    }
-                }
                 b'(' => self.tokens.push(Token::LParen { line: self.line }),
                 b')' => self.tokens.push(Token::RParen { line: self.line }),
                 b'{' => self.tokens.push(Token::LCurly { line: self.line }),
@@ -40,7 +37,15 @@ impl<'lex> Lexer<'lex> {
                 b'.' => self.tokens.push(Token::Dot { line: self.line }),
                 b',' => self.tokens.push(Token::Comma { line: self.line }),
                 b'+' => self.tokens.push(Token::Plus { line: self.line }),
-                b'-' => self.tokens.push(Token::Minus { line: self.line }),
+                b'-' => {
+                    if self.match_on(b'-') {
+                        while self.peek().is_some_and(|c| c != b'\n') {
+                            self.advance();
+                        }
+                    } else {
+                        self.tokens.push(Token::Minus { line: self.line });
+                    }
+                }
                 b'*' => self.tokens.push(Token::Star { line: self.line }),
                 b'^' => self.tokens.push(Token::Caret { line: self.line }),
                 b'/' => self.tokens.push(Token::Slash { line: self.line }),
@@ -84,62 +89,87 @@ impl<'lex> Lexer<'lex> {
                         self.tokens.push(Token::Colon { line: self.line });
                     }
                 }
-                b'0'..=b'9' => {
-                    while self.peek().is_ascii_digit() {
-                        self.advance();
-                    }
-                    if self.peek() == b'.' && self.lookahead(1).is_some_and(|x| x.is_ascii_digit())
-                    {
-                        self.advance();
-                        while self.peek().is_ascii_digit() {
-                            self.advance();
-                        }
-                        self.tokens.push(Token::FloatLiteral {
-                            lexeme: Lexeme::new(&self.source[self.start..self.curr]),
-                            line: self.line,
-                        });
-                    } else {
-                        self.tokens.push(Token::IntegerLiteral {
-                            lexeme: Lexeme::new(&self.source[self.start..self.curr]),
-                            line: self.line,
-                        });
-                    }
-                }
-                b'\'' => {
-                    while self.peek() != b'\'' && self.curr < self.source.len() {
-                        self.advance();
-                    }
-                    self.advance();
-                    self.tokens.push(Token::StringLiteral {
-                        lexeme: Lexeme::new(&self.source[self.start..self.curr]),
-                        line: self.line,
-                    });
-                }
-                b'"' => {
-                    while self.peek() != b'"' && self.curr < self.source.len() {
-                        self.advance();
-                    }
-                    self.advance();
-                    self.tokens.push(Token::StringLiteral {
-                        lexeme: Lexeme::new(&self.source[self.start..self.curr]),
-                        line: self.line,
-                    });
-                }
-                _ => (),
+                b'0'..=b'9' => self.handle_number_literal(),
+                b'\'' => self.handle_string_literal(b'\''),
+                b'"' => self.handle_string_literal(b'"'),
+                b'a'..=b'z' | b'A'..=b'Z' | b'_' => self.handle_identifier(),
+                _ => panic!("[Line {}] Unexpected character: '{}'", self.line, c as char),
             }
         }
 
         &self.tokens
     }
 
-    pub fn advance(&mut self) -> u8 {
-        let ch = self.source[self.curr];
-        self.curr += 1;
-        ch
+    fn handle_identifier(&mut self) {
+        while self
+            .peek()
+            .is_some_and(|c| c.is_ascii_alphanumeric() || c == b'_')
+        {
+            self.advance();
+        }
+        if let Some(keyword) =
+            match_reserved_keyword(&self.source[self.start..self.curr], self.line)
+        {
+            self.tokens.push(keyword);
+        } else {
+            self.tokens.push(Token::Identifier {
+                lexeme: Lexeme::new(&self.source[self.start..self.curr]),
+                line: self.line,
+            });
+        }
     }
 
-    pub fn peek(&self) -> u8 {
-        self.source[self.curr]
+    fn handle_number_literal(&mut self) {
+        while self.peek().is_some_and(|c| c.is_ascii_digit()) {
+            self.advance();
+        }
+        if self.peek().is_some_and(|c| c == b'.')
+            && self.lookahead(1).is_some_and(|x| x.is_ascii_digit())
+        {
+            self.advance();
+            while self.peek().is_some_and(|c| c.is_ascii_digit()) {
+                self.advance();
+            }
+            self.tokens.push(Token::FloatLiteral {
+                lexeme: Lexeme::new(&self.source[self.start..self.curr]),
+                line: self.line,
+            });
+        } else {
+            self.tokens.push(Token::IntegerLiteral {
+                lexeme: Lexeme::new(&self.source[self.start..self.curr]),
+                line: self.line,
+            });
+        }
+    }
+
+    fn handle_string_literal(&mut self, quote: u8) {
+        while self.peek().is_some_and(|c| c != quote && c != b'\n') {
+            self.advance();
+        }
+        if self.curr >= self.source.len() || self.peek().is_some_and(|c| c == b'\n') {
+            panic!("[Line {}] Unterminated string literal", self.line);
+        }
+        self.advance();
+        self.tokens.push(Token::StringLiteral {
+            lexeme: Lexeme::new(&self.source[self.start..self.curr]),
+            line: self.line,
+        });
+    }
+
+    pub fn advance(&mut self) -> Option<u8> {
+        if self.curr >= self.source.len() {
+            return None;
+        }
+        let c = self.source[self.curr];
+        self.curr += 1;
+        Some(c)
+    }
+
+    pub fn peek(&self) -> Option<u8> {
+        if self.curr >= self.source.len() {
+            return None;
+        }
+        Some(self.source[self.curr])
     }
 
     pub fn lookahead(&self, n: usize) -> Option<u8> {
@@ -158,5 +188,27 @@ impl<'lex> Lexer<'lex> {
         }
         self.curr += 1;
         true
+    }
+}
+
+pub fn match_reserved_keyword(token: &[u8], line: usize) -> Option<Token<'_>> {
+    match token {
+        b"if" => Some(Token::If { line }),
+        b"else" => Some(Token::Else { line }),
+        b"then" => Some(Token::Then { line }),
+        b"true" => Some(Token::True { line }),
+        b"false" => Some(Token::False { line }),
+        b"and" => Some(Token::And { line }),
+        b"or" => Some(Token::Or { line }),
+        b"while" => Some(Token::While { line }),
+        b"do" => Some(Token::Do { line }),
+        b"for" => Some(Token::For { line }),
+        b"func" => Some(Token::Func { line }),
+        b"null" => Some(Token::Null { line }),
+        b"end" => Some(Token::End { line }),
+        b"print" => Some(Token::Print { line }),
+        b"println" => Some(Token::Println { line }),
+        b"ret" => Some(Token::Ret { line }),
+        _ => None,
     }
 }
