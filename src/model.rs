963a5dee5f129a52d4e0cb1f5104309655214939
Arthur Bied-Charreton <arthur@biedcharreton.com>
2026-01-18 00:17:38 +0100

    Add pretty printing for Stmts

--- src/model.rs
+++ src/model.rs
@@ -22,14 +22,14 @@ pub enum Expr {
     LogicalOp(LogicalOp),
 }
 
-#[derive(Clone, Debug)]
+#[derive(Clone)]
 pub enum Stmt {
     Print(Print),
     Println(Println),
     If(If),
 }
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct Stmts {
     stmts: Vec<Stmt>,
 }
@@ -44,36 +44,37 @@ impl Stmts {
     }
 }
 
-fn dump_ast(ast: &Expr, f: &mut std::fmt::Formatter<'_>, indentation: Option<usize>) -> std::fmt::Result {
+fn dump_expr(expr: &Expr, f: &mut std::fmt::Formatter<'_>, indentation: Option<usize>) -> std::fmt::Result {
     let indentation = indentation.unwrap_or(0);
-    match ast {
+
+    match expr {
         Expr::Integer(int) => writeln!(f, "{}{int:?}", " ".repeat(indentation)),
         Expr::Float(float) => writeln!(f, "{}{float:?}", " ".repeat(indentation)),
         Expr::Bool(bool) => writeln!(f, "{}{bool:?}", " ".repeat(indentation)),
         Expr::String(str) => writeln!(f, "{}{str:?}", " ".repeat(indentation)),
         Expr::Grouping(group) => {
             writeln!(f, "{}Grouping (", " ".repeat(indentation))?;
-            dump_ast(group, f, Some(indentation + 4))?;
+            dump_expr(group, f, Some(indentation + 4))?;
             writeln!(f, "{}) // grouping", " ".repeat(indentation))
         }
         Expr::UnOp(unop) => {
             writeln!(f, "{}UnOp {{", " ".repeat(indentation))?;
             writeln!(f, "{}{:?}", " ".repeat(indentation + 4), unop.operator)?;
-            dump_ast(&unop.operand, f, Some(indentation + 4))?;
+            dump_expr(&unop.operand, f, Some(indentation + 4))?;
             writeln!(f, "{}}} // unop", " ".repeat(indentation))
         }
         Expr::BinOp(binop) => {
             writeln!(f, "{}BinOp {{", " ".repeat(indentation))?;
-            dump_ast(&binop.lhs, f, Some(indentation + 4))?;
+            dump_expr(&binop.lhs, f, Some(indentation + 4))?;
             writeln!(f, "{}{:?}", " ".repeat(indentation + 4), binop.operator)?;
-            dump_ast(&binop.rhs, f, Some(indentation + 4))?;
+            dump_expr(&binop.rhs, f, Some(indentation + 4))?;
             writeln!(f, "{}}} // binop", " ".repeat(indentation))
         }
         Expr::LogicalOp(logicalop) => {
             writeln!(f, "{}LogicalOp {{", " ".repeat(indentation))?;
-            dump_ast(&logicalop.lhs, f, Some(indentation + 4))?;
+            dump_expr(&logicalop.lhs, f, Some(indentation + 4))?;
             writeln!(f, "{}{:?}", " ".repeat(indentation + 4), logicalop.operator)?;
-            dump_ast(&logicalop.rhs, f, Some(indentation + 4))?;
+            dump_expr(&logicalop.rhs, f, Some(indentation + 4))?;
             writeln!(f, "{}}} // logicalop", " ".repeat(indentation))
         }
     }
@@ -81,7 +82,53 @@ fn dump_ast(ast: &Expr, f: &mut std::fmt::Formatter<'_>, indentation: Option<usi
 
 impl std::fmt::Debug for Expr {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        dump_ast(self, f, None)
+        dump_expr(self, f, None)
+    }
+}
+
+fn dump_stmt(stmt: &Stmt, f: &mut std::fmt::Formatter<'_>, indentation: Option<usize>) -> std::fmt::Result {
+    let indentation = indentation.unwrap_or(0);
+
+    match stmt {
+        Stmt::If(stmt) => {
+            writeln!(f, "{}if", " ".repeat(indentation))?;
+            dump_expr(&stmt.test, f, Some(indentation + 4))?;
+            writeln!(f, "{}then", " ".repeat(indentation))?;
+            dump_stmts(stmt.then(), f, Some(indentation + 4))?;
+            if let Some(elsee) = stmt.r#else() {
+                writeln!(f, "{}else", " ".repeat(indentation))?;
+                dump_stmts(elsee, f, Some(indentation + 4))?;
+            }
+            writeln!(f, "end")
+        }
+        Stmt::Print(stmt) => {
+            writeln!(f, "{}print", " ".repeat(indentation))?;
+            dump_expr(stmt.expr(), f, Some(indentation + 4))
+        }
+        Stmt::Println(stmt) => {
+            writeln!(f, "{}println", " ".repeat(indentation))?;
+            dump_expr(stmt.expr(), f, Some(indentation + 4))
+        }
+    }
+}
+
+impl std::fmt::Debug for Stmt {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        dump_stmt(self, f, None)
+    }
+}
+
+fn dump_stmts(stmts: &Stmts, f: &mut std::fmt::Formatter<'_>, indentation: Option<usize>) -> std::fmt::Result {
+    let indentation = indentation.unwrap_or(0);
+    for stmt in &stmts.stmts {
+        writeln!(f, "{}{stmt:?}", " ".repeat(indentation))?;
+    }
+    Ok(())
+}
+
+impl std::fmt::Debug for Stmts {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        dump_stmts(self, f, None)
     }
 }
 
