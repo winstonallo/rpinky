2ba75866fa8c61fbf1007e19e5aabfe1a2e5690d
Arthur Bied-Charreton <arthur@biedcharreton.com>
2026-01-18 12:34:16 +0100

    Restructure imports in visitor.rs

--- src/visitor.rs
+++ src/visitor.rs
@@ -1,20 +1,20 @@
-use crate::model::{BinOp, Bool, Float, If, Integer, LogicalOp, Print, Println, StringType, UnOp};
+use crate::model;
 
 /// Visitor trait for expression nodes.
 pub trait ExprVisitor<T> {
-    fn visit_integer(&mut self, n: &Integer) -> T;
-    fn visit_float(&mut self, f: &Float) -> T;
-    fn visit_string(&mut self, s: &StringType) -> T;
-    fn visit_bool(&mut self, b: &Bool) -> T;
-    fn visit_grouping(&mut self, inner: &crate::model::Expr) -> T;
-    fn visit_unop(&mut self, op: &UnOp) -> T;
-    fn visit_binop(&mut self, op: &BinOp) -> T;
-    fn visit_logical(&mut self, op: &LogicalOp) -> T;
+    fn visit_integer(&mut self, n: &model::Integer) -> T;
+    fn visit_float(&mut self, f: &model::Float) -> T;
+    fn visit_string(&mut self, s: &model::StringType) -> T;
+    fn visit_bool(&mut self, b: &model::Bool) -> T;
+    fn visit_grouping(&mut self, inner: &model::Expr) -> T;
+    fn visit_unop(&mut self, op: &model::UnOp) -> T;
+    fn visit_binop(&mut self, op: &model::BinOp) -> T;
+    fn visit_logical(&mut self, op: &model::LogicalOp) -> T;
 }
 
 /// Visitor trait for statement nodes.
 pub trait StmtVisitor<T> {
-    fn visit_print(&mut self, p: &Print) -> T;
-    fn visit_println(&mut self, p: &Println) -> T;
-    fn visit_if(&mut self, i: &If) -> T;
+    fn visit_print(&mut self, p: &model::Print) -> T;
+    fn visit_println(&mut self, p: &model::Println) -> T;
+    fn visit_if(&mut self, i: &model::If) -> T;
 }
